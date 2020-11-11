c5037ee104a9560da2f3622563463d83181633f0
davis <130086230@qq.com>
2020-11-11 18:09:58 +0800

    up

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fanxinyu
+ * Date: 2020-11-11
+ * Time: 18:05
+ */
+namespace Daviswwang\MemberIn\Exceptions;
+
+class Exception extends \Exception{}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fanxinyu
+ * Date: 2020-11-11
+ * Time: 18:09
+ */
+namespace Daviswwang\MemberIn\Exceptions;
+
+class HttpException extends Exception
+{
+
+}
